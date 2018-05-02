3d312517051a00e5efb031fa0d6a0225f2b461a5
swetttirexkiller <piotrjankiewicz02@gmail.com>
2018-05-02 14:12:32 +0200

    understood clusters and workers

--- index.js
+++ index.js
@@ -1,25 +1,23 @@
+process.env.UV_THREADPOOL_SIZE = 1;
 const cluster = require('cluster');
+const crypto = require('crypto');
 
 //is the file executed in the master mode ?
 if (cluster.isMaster) {
     // Cause index.js to be executed *again* but in slave mode
     cluster.fork();
-    // cluster.fork();
-    // cluster.fork();
-    // cluster.fork();
+    cluster.fork();
+    cluster.fork();
+    cluster.fork();
 } else {
     // I am a child, Im going to act like a server an nothing else
     const express = require('express');
     const app = express();
 
-    function doWork(duration) {
-        const start = Date.now();
-        while (Date.now() - start < duration) {}
-    }
-
     app.get('/', (req, res) => {
-        doWork(5000);
-        res.send('Hi there!');
+        crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+            res.send('Hi there!');
+        });
     });
 
     app.get('/fast', (req,res)=>{
