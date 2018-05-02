6221299ad3a3bd7b56f5df3418643f651ddbfebe
swetttirexkiller <piotrjankiewicz02@gmail.com>
2018-05-02 13:08:05 +0200

    example of clustering

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
-const express = require('express');
-const app = express();
+const cluster = require('cluster');
 
-function doWork(duration){
-    const start = Date.now();
-    while(Date.now() - start < duration){}
+//is the file executed in the master mode ?
+if (cluster.isMaster) {
+    // Cause index.js to be executed *again* but in slave mode
+    cluster.fork();
+    // cluster.fork();
+    // cluster.fork();
+    // cluster.fork();
+} else {
+    // I am a child, Im going to act like a server an nothing else
+    const express = require('express');
+    const app = express();
+
+    function doWork(duration) {
+        const start = Date.now();
+        while (Date.now() - start < duration) {}
+    }
+
+    app.get('/', (req, res) => {
+        doWork(5000);
+        res.send('Hi there!');
+    });
+
+    app.get('/fast', (req,res)=>{
+        res.send('this was fast!');
+    });
+
+    app.listen(3000);
+
 }
-
-app.get('/', (req, res) => {
-    doWork(5000);
-    res.send('Hi there!');
-});
-
-app.listen(3000);
